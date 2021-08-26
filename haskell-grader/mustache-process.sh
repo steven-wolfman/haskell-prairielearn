566bd8656ecb5e61b229645f3be293b8c65502ce
Steve Wolfman <steven.wolfman@gmail.com>
2021-08-26 09:45:13 -0700

    Substantially improved the mustache processor.

--- haskell-grader/mustache-process.sh
+++ haskell-grader/mustache-process.sh
@@ -1,24 +1,95 @@
 #!/usr/bin/env bash
+
+# Mustache processing intended primarily for use with PrairieLearn external graders.
+# Bash script template based on https://betterdev.blog/minimal-safe-bash-script-template/
+
+set -Eeuo pipefail
 shopt -s globstar
 
-# Plan:
-# + Takes as a parameter the name of a .json file to use for mustache formatting. Defaults to /grade/data/data.json
-#   Call this DATA_SOURCE
-# + Locate any file under the current directory that ends in .mustache; for each such file {}:
-#   + run mustache ${DATA_SOUCE} {} > $(basename {} .mustache)
-
 DEFAULT_DATA_SOURCE=/grade/data/data.json
-DATA_SOURCE=${1-$DEFAULT_DATA_SOURCE}
+DATA_SOURCE="${DEFAULT_DATA_SOURCE}"
 
-if [[ ! -x "${DATA_SOURCE}" ]]
+USAGE=$(cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [<formatter>]
+
+Mustache expansion for files in the current subtree with the extension .mustace.
+Results are placed in files with matching paths but without the .mustache extension.
+Formats using <formatter> (defaulting to "${DEFAULT_DATA_SOURCE}").
+
+Assumes the availability of 'mustache' program on path. Does not process files
+named exactly '.mustache'. The subtree rooted at CWD is defined by the "**" pattern.
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info (bash -x option)
+EOF
+)
+
+usage() {
+  echo "${USAGE}"
+  exit
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+dieUI() {
+  local msg=$1
+  local code=${2-1}
+  msg "$msg"
+  msg "\n${USAGE}"
+  exit "$code"
+}
+
+parse_params() {
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    -?*) dieUI "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  # Optional argument
+  if [[ $# -gt 0 ]]
+  then
+    DATA_SOURCE="${1-}"
+    shift
+  else
+    msg "Using default formatter at: ${DATA_SOURCE}"
+  fi
+
+  # Check for disallowed arguments:
+  [[ $# -gt 0 ]] && dieUI "Too many arguments provided"
+
+  return 0
+}
+
+parse_params "$@"
+
+# script logic here
+
+if [[ ! -f "${DATA_SOURCE}" ]]
 then
-    echo "Error: Data source not found at \"${DATA_SOURCE}\""
-    exit 1
+    die "No file found at: ${DATA_SOURCE}"
 fi
+
+msg "Using formatter: ${DATA_SOURCE}"
 
 for i in **/?*.mustache
 do
-    echo "Processing ${i} using ${DATA_SOURCE}."
     basei="${i%.mustache}"
+    msg "Processing ${i}"
     mustache "${DATA_SOURCE}" "${i}" > "${basei}"
 done
